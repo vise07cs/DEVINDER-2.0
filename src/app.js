5003982cf0914e6bbc319d41627facbf34f6b595
vise07cs <vikram7cs@gmail.com>
2025-10-01 17:14:39 +0530

    new mongoDB cluster created + mongoose library + connect app to the DB

--- src/app.js
+++ src/app.js
@@ -4,44 +4,25 @@ console.log("App is starting...");
 const express=require('express');
 const app=express();
 const {authMiddleware,userMiddleware}=require('../middlewares/auth');
+const connectDB = require('./config/db');
 
 
-
-
-
-
-app.use("/admin",authMiddleware);
-
-
-app.get("/admin/getData",(req,res)=>{
-    res.send("This is the admin data we got after authentication");
+connectDB()
+  .then(() => {
+    console.log(" Database connected successfully ");
+    app.listen(3003,()=>{
+    console.log("server is running on port 3003");
 });
 
-app.get("/admin/deleteData",(req,res)=>{
-    res.send("This is delete data  we got after authentication");
-});
+  })
+  .catch((err) => {
+    console.log("❌ DB cannot be connected", err);
+  });
 
 
-app.get("/user/data",userMiddleware, (req,res)=>{
-    res.send("This is user data that anyone can access");
-});
 
 
 
  
 
-app.listen(3003,()=>{
-    console.log("server is running on port 3003");
-}); 
 
-
-// The current code flow 
-// Request hits Express.
-
-// Middleware,  authMiddleware runs (app.use("/admin", authMiddleware)).
-
-// Middleware calls next().
-
-// Express finds the matching route (app.get("/admin/deleteData")).
-
-// That route sends the response.
---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://vikram07cs_db_user:3mE3kWNY37ev5kFT@devtinde2.sexnzuf.mongodb.net/hello");
+};
+
+module.exports = connectDB;
+
