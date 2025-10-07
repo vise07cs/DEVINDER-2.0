ee5dd5d34f3e6ecc7ad0702e19549aa49573bb39
vise07cs <vikram7cs@gmail.com>
2025-10-07 14:22:40 +0530

    userAuth middleware added , sendConnection request APi created

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,43 @@
-function authMiddleware(req,res,next){
+const JWT = require("jsonwebtoken");
+    const User = require("../src/models/user");
+
+
+
+const userAuth=async function userAuth(req,res,next){
+    // read token from cookies , validate the token and  find the user
+
+
+    try{
+
+    
     console.log("Authenticating the user via auth middleware...");
-    const token = "abc";
-    const isTokenValid = token === "abc";
+    const {token} = req.cookies
+    if(!token){
+        return res.status(401).send("Unauthorized access, token not available");
+    }
+    // validate token
+    const isTokenValid = JWT.verify(token, "dev@tinder");
     if(!isTokenValid){
-        res.status(403).send("Unauthorized token");
-
-    }
-    else{
-        next();
+        return res.status(401).send("Unauthorized access, invalid token");
     }
 
+    const {id} = isTokenValid;
+    const user = await User.findById(id);
+    if(!user){
+        return res.status(404).send("User not found");
+    }
+    req.user=user;
+    next();
+   
+
+}catch(err){
+    return res.status(400).send(err.message);
+}
 }
 
-function userMiddleware(req,res,next){
-    console.log("Authenticating the user via user middleware...");
-    const token = "abc";
-    const isTokenValid = token === "abc";
-    if(!isTokenValid){
-        res.status(403).send("Unauthorized token");
-
-    }
-    else{
-        next();
-    }
-
-}
 
 
 
 
 
-module.exports={authMiddleware,userMiddleware};
+module.exports={userAuth};
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ console.log("App is starting...");
 
 const express=require('express');
 const app=express();
-const {authMiddleware,userMiddleware}=require('../middlewares/auth');
+const {userAuth}=require('../middlewares/auth');
 const connectDB = require('./config/db');
 const User=require('./models/user');
 const {validateSignupData}=require('./utils/validation');
@@ -59,9 +59,10 @@ app.post("/login",async(req,res)=>{
        throw new Error("User not found");
     }
     // create JWT token
-    const token=JWT.sign({id:user._id},"dev@tinder");
+    const token=JWT.sign({id:user._id},"dev@tinder",{expiresIn:"1d"});
+
     // console.log(token);
-    res.cookie("token",token);
+    res.cookie("token",token ,{expires:new Date(Date.now()+ 86400000)});
 
     // add token in cookie and send response back to the user
 
@@ -76,27 +77,9 @@ app.post("/login",async(req,res)=>{
 })
 
 // get current user profile
-app.get("/profile",async (req,res)=>{
+app.get("/profile",userAuth, async (req,res)=>{
   try{
-    const {token}=req.cookies;
-    if(!token){
-      return res.status(401).send("Unauthorized access , token not available");
-    }
-
-    // validate token
-    const isTokenValid=JWT.verify(token,"dev@tinder");
-    if(!isTokenValid){
-      return res.status(401).send("Unauthorized access");
-    }
-
-    console.log(isTokenValid);
-    const {id}=isTokenValid;
-    const user=await User.findById(id);
-    if(!user){
-      return res.status(404).send("User not found");
-    }
-    // console.log(user);
-  
+    const user=req.user
     res.send(user);
   }catch(err){
     return res.status(400).send(err.message);
@@ -106,76 +89,14 @@ app.get("/profile",async (req,res)=>{
 )
 
 
-// Get all users from the database
-app.get("/feed",async (req,res)=>{
-;
+app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
   try{
-    const user=await User.find({});
-    if(user.length===0){
-      return res.status(404).send("User not found");
-    }
-    res.send(user);
+    const user=req.user;
+        res.send("Connection request sent successfully by "+ user.firstName);
+
   }catch(err){
-    console.log("Error in fetching users from db",err);
-    res.status(500).send("Internal server error");
-  } 
+    return res.status(400).send(err.message);
 
-})
-
-
-// Get a single user from the database by email
-app.get("/user",async (req,res)=>{
-
-  try{
-    const user=await User.findOne({email:req.body.email});
-    if(!user){
-      return res.status(404).send("User not found");
-    }
-    res.send(user);
-  }catch(err){
-    console.log("Error in fetching users from db",err);
-    res.status(500).send("Internal server error");
-  } 
-
-})
-
-
-// Delete a user from the database by email
-
-app.delete("/user",async (req,res)=>{
-  try{
-    const user=await User.findOneAndDelete({email:req.body.email});
-    if(!user){
-      return res.status(404).send("User not found");
-    }
-    res.send("User deleted successfully");
-  }catch(err){
-    console.log("Error in deleting user from db",err);
-    res.status(500).send("Internal server error");
-  }
-
-})
-
-// update a user from the database by email
-
-app.put("/user",async (req,res)=>{
-  try{ 
-
-    const allowedUpdates=["about","skills","age","photoURL"];
-    const updates=Object.keys(req.body);
-    const isValidOperation=updates.every((update)=>allowedUpdates.includes(update));
-    if(!isValidOperation){
-      return res.status(400).send("Invalid updates!");
-    }
-    
-    const user=await User.findOneAndUpdate({email:req.query.email},req.body,{new:true,runValidators:true});
-    if(!user){
-      return res.status(404).send("User not found");
-    } 
-    res.send(user);
-  }catch(err){  
-    console.log("Error in updating user from db",err);
-    res.status(500).send("Internal server error");
   }
 })
 
