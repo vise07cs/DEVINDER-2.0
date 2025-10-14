5e9251c035b0fd77beab30ac7433a7b8b33fced9
vise07cs <vikram7cs@gmail.com>
2025-10-14 22:00:32 +0530

    Request review  API added

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -65,6 +65,54 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
   }
 })
 
+
+// review connection request (accept/reject)
+requestRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+  try{
+    const loggedInUser=req.user;   //He is the logged in user (we are getting him from auth middleware). Although we are not using this variable but it is useful for understanding
+    const loggedInUserId=req.user._id;
+    const requestId=req.params.requestId;
+    const status=req.params.status;
+    const allowedStatuses=["accepted","rejected"];
+
+
+
+    if(!allowedStatuses.includes(status)){
+      throw new Error("Status must be either accepted or rejected");
+    }
+
+
+
+
+    const connectionRequest=await ConnectionRequestModel.findById(requestId);
+    if(!connectionRequest){
+      throw new Error("The connection request you are trying to review does not exist");
+    }
+
+    // only the toUserId can review the connection request as he is the one who received it and is the logged in user
+    if(connectionRequest.toUserId.toString()!==loggedInUserId.toString()){
+      throw new Error("You are not authorized to review this connection request");
+    }
+    if(connectionRequest.status==="accepted" || connectionRequest.status==="rejected"){
+      throw new Error("This connection request has already been reviewed");
+    }
+    if(!connectionRequest.status==="interested"){
+      throw new Error("This connection request is not in interested status so it cannot be reviewed");
+    }
+
+    connectionRequest.status=status;
+
+    const data=await connectionRequest.save();
+    res.json({
+      message:`Connection request  has been ${status} by ${loggedInUser.firstName}`,
+      data:data
+    })
+  }catch(err){
+    return res.status(400).send(err.message);
+    
+  }
+})
+
 module.exports={requestRouter};
 
 
